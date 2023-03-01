471c5f44c62316505898dc3b6333cd6dcb0d7cab
checkiamsiam <issiam02415@gmail.com>
2023-03-02 00:13:17 +0600

    file type declarations

--- src/components/common/SEO.tsx
+++ src/components/common/SEO.tsx
@@ -1,15 +1,12 @@
 import Head from "next/head";
 
-function SEO({
-  title = "Web Serv | Developer Agency",
-  description = "web serv - a web development service provider agency",
-}) {
+function SEO({ title = "Web Serv | Developer Agency", description = "web serv - a web development service provider agency", icon = "/favicon.ico" }) {
   return (
     <Head>
       <title>{title}</title>
       <meta name="description" content={description} />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href={icon} />
     </Head>
   );
 }
--- src/pages/maintenance.tsx
+++ src/pages/maintenance.tsx
@@ -1,7 +1,7 @@
-import SEO from "@/components/common/SEO";
-import Image from "next/image";
-import { Box, Link, Stack, Typography } from "@mui/material";
 import logo_bg from "@/assets/images/logo-with-bg.png";
+import SEO from "@/components/common/SEO";
+import { Box, Link, Stack, Typography } from "@mui/material";
+import Image from "next/image";
 
 export default function maintenance() {
   return (
@@ -15,13 +15,7 @@ export default function maintenance() {
         sx={{ minHeight: "100vh", px: { xs: 2, md: 10 } }}
       >
         <Box width="100%" sx={{ textAlign: "center" }}>
-          <Image
-            src={logo_bg}
-            alt="maintenance"
-            priority
-            width={500}
-            height={500}
-          />
+          <Image src={logo_bg} alt="maintenance" priority width={500} height={500} />
         </Box>
 
         <Box>
@@ -29,8 +23,7 @@ export default function maintenance() {
             Site Under Maintenance
           </Typography>
           <Typography variant="body2">
-            Sorry for the inconvenience but we&rsquo;re performing some
-            maintenance at the moment. If you need to you can always{" "}
+            Sorry for the inconvenience but we&rsquo;re performing some maintenance at the moment. If you need to you can always{" "}
             <Link
               href="mailto:webserv23@gmail.com"
               target="_blank"
