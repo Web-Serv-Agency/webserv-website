fac11c4d14eac0ac5a6b5d220e730250d3680195
Mohammad Ali <itsproali@gmail.com>
2023-04-12 03:17:32 +0600

    Loading added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,33 @@
+import Loading from "@/components/common/Loading";
 import "@/styles/globals.css";
 import store, { persistor } from "@/utils/store";
 import ThemeProvider from "@/utils/theme";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import Router from "next/router";
+import { useEffect, useState } from "react";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    const startLoading = () => setLoading(true);
+    const stopLoading = () => setLoading(false);
+
+    Router.events.on("routeChangeStart", startLoading);
+    Router.events.on("routeChangeComplete", stopLoading);
+    Router.events.on("routeChangeError", stopLoading);
+
+    return () => {
+      Router.events.off("routeChangeStart", startLoading);
+      Router.events.off("routeChangeComplete", stopLoading);
+      Router.events.off("routeChangeError", stopLoading);
+    };
+  }, []);
+
   return (
     <>
       {/* Default Head */}
@@ -26,7 +45,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <ThemeProvider>
-            <Component {...pageProps} />
+            {loading ? <Loading /> : <Component {...pageProps} />}
           </ThemeProvider>
         </PersistGate>
       </Provider>
