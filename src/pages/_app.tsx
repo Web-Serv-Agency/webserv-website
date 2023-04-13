2d9217497ab6fb410e994c78044b3112d396d5cf
checkiamsiam <issiam02415@gmail.com>
2023-04-13 20:01:34 +0600

    code refactor

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,40 +1,19 @@
-import Loading from "@/components/common/Loading";
+import RouteChangeWrapper from "@/components/common/RouteChangeWrapper";
 import { store } from "@/features/app/store";
 import "@/styles/globals.css";
 import ThemeProvider from "@/utils/theme";
 import { AnimatePresence } from "framer-motion";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import Router from "next/router";
-import { useEffect, useState } from "react";
 import { Provider } from "react-redux";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [loading, setLoading] = useState(false);
-  useEffect(() => {
-    const startLoading = () => setLoading(true);
-    const stopLoading = () => setLoading(false);
-
-    Router.events.on("routeChangeStart", startLoading);
-    Router.events.on("routeChangeComplete", stopLoading);
-    Router.events.on("routeChangeError", stopLoading);
-
-    return () => {
-      Router.events.off("routeChangeStart", startLoading);
-      Router.events.off("routeChangeComplete", stopLoading);
-      Router.events.off("routeChangeError", stopLoading);
-    };
-  }, []);
-
   return (
     <>
       {/* Default Head */}
       <Head>
         <title>Web Serv | Developer Agency</title>
-        <meta
-          name="description"
-          content="web serv - a web development service provider agency"
-        />
+        <meta name="description" content="web serv - a web development service provider agency" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
@@ -43,7 +22,9 @@ export default function App({ Component, pageProps }: AppProps) {
       <Provider store={store}>
         <ThemeProvider>
           <AnimatePresence>
-            {loading ? <Loading/> : <Component {...pageProps} />}
+            <RouteChangeWrapper>
+              <Component {...pageProps} />
+            </RouteChangeWrapper>
           </AnimatePresence>
         </ThemeProvider>
       </Provider>
